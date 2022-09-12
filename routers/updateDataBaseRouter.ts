95d9d3a588c269c9a9e82ffc722735cad37c1d17
Iwo Matecki <iwo@iwomi.pl>
2022-09-12 12:44:29 +0200

    refactor code of updateDataBaseRouter created separate function to update db for two existing routes

--- routers/updateDataBaseRouter.ts
+++ routers/updateDataBaseRouter.ts
@@ -1,9 +1,8 @@
 import {Router} from 'express';
 import {topMoviesFromFile} from "../utils/topMoviesFromFile";
-import {fetchFunction} from "../utils/fetchFunction";
-import {MoviesFromOMDBAPI, NotAddedMovie, TopMoviesInterface} from "../types";
+import {TopMoviesInterface} from "../types";
 import {TopMovie} from "../records/topMovies.record";
-import {addUnsuccessfulMovieAddingToFIle} from "../utils/update-functions-utils";
+import {updateMoviesListInDb} from "../utils/update-functions-utils";
 
 export const updateDataBaseRouter = Router();
 
@@ -12,109 +11,19 @@ updateDataBaseRouter
     .post('/', async (req, res) => {
       const topMovies: TopMoviesInterface[] = req.body;
 
-      /**---------- map the list of movies ------------*/
-      await Promise.all(topMovies.map(async (movie) => {
-          const {position, origTitle, polTitle, year, imgOfMovie} = movie;
-          /**---------- check if movie is in my DB ------------*/
-          const movieFromDB = year && (origTitle || polTitle) ? await TopMovie.checkIfItIsInDataBase(new Date(year), origTitle, polTitle): null;
-          /**---------- if it is, update only the position  ------------*/
-          if(movieFromDB){
-                return movieFromDB.position !== Number(position) ?
-                  await TopMovie.updateMoviePosition(movieFromDB.id, Number(position), movieFromDB.origTitle) :
-                  origTitle;
-          } else {
-
-              /**---------- if movie is not in DB fetch movie data from OMDB API ------------*/
-          let movieFromOMDB = (await fetchFunction('GET', `${encodeURIComponent(movie.origTitle || movie.polTitle)}`, `${movie.year}`) as unknown as MoviesFromOMDBAPI);
-
-          /**---------- if movie is in OMDB DB ------------*/
-          if (movieFromOMDB !== null) {
-              const {
-                  Title: title, //lacking of title usage from this source, I'm going to use it to check if data from OMDBAPI is correct
-                  Rated: rated,
-                  Genre: genre,
-                  Director: director,
-                  Actors: actors,
-                  Plot: plot,
-                  Poster: poster
-              } = movieFromOMDB;
-
-              /**---------- create instance of TomMovie class ------------*/
-              const newMovie = new TopMovie({
-                  rated,
-                  genre,
-                  director,
-                  actors,
-                  plot,
-                  poster,
-                  position: Number(position),
-                  origTitle: origTitle || polTitle,
-                  polTitle: polTitle || origTitle,
-                  year: new Date(year),
-                  imgOfMovie,
-              });
-
-              /**---------- and save this instance on my DB and return the title or undefined if it is not saved ------------*/
-              return await newMovie.addNewMovieToDataBase() as string | undefined;
-          }
-
-              /**---------- if above could not be returned add this movie to list of unsuccessful addings ------------*/
-              await addUnsuccessfulMovieAddingToFIle({
-                  origTitle: movie.origTitle,
-                  polTitle: movie.polTitle,
-              } as NotAddedMovie);
-          return;
-          }
-      }
-      ));
-        /**---------- after mapping set and store the list with names and number of occurrences of genres in the list ------------*/
-        await TopMovie.setGenresList();
+      /**---------- map and save the list of movies ------------*/
+      await updateMoviesListInDb(topMovies);
+      /**---------- after mapping set and store the list with names and number of occurrences of genres in the list ------------*/
+       await TopMovie.setGenresList();
 
   })
   /** ------------updating movies list from file topMoviesFromFile-------*/
-  /** ------------NOT TO BE USED-------*/
   .get('/updateMovies', async (req, res) => {
-      await Promise.all(topMoviesFromFile.map(async (movie) => {
-          await setTimeout(async () => {
-              const {position, origTitle, polTitle, year, imgOfMovie} = movie;
-              let movieFromOMDB = await fetchFunction('GET', `${encodeURIComponent(movie.origTitle || movie.polTitle)}`, `${movie.year}`) as unknown as MoviesFromOMDBAPI;
 
-              // movieFromOMDB = movieFromOMDB !== null ? movieFromOMDB : await fetchFunction('GET', `${encodeURIComponent(movie.polTitle)}`, `${movie.year}`) as unknown as MoviesFromOMDBAPI;
-
-              if (movieFromOMDB !== null ) {
-                  const {
-                      Title: title, //lacking of title usage from this source, I'm going to use it to check if data from OMDBAPI is correct
-                      Rated: rated,
-                      Genre: genre,
-                      Director: director,
-                      Actors: actors,
-                      Plot: plot,
-                      Poster: poster
-                  } = movieFromOMDB;
-
-                  const newMovie = new TopMovie({
-                      rated,
-                      genre,
-                      director,
-                      actors,
-                      plot,
-                      poster,
-                      position: Number(position),
-                      origTitle: origTitle || polTitle,
-                      polTitle: polTitle || origTitle,
-                      year: new Date(year),
-                      imgOfMovie,
-                  })
-
-                  return await newMovie.addNewMovieToDataBase();
-              }
-
-              await addUnsuccessfulMovieAddingToFIle({
-                  origTitle: movie.origTitle,
-                  polTitle: movie.polTitle,
-              } as NotAddedMovie);
-          }, Math.random() * 15000);
-      }))
+      /**---------- map and save the list of movies ------------*/
+      await updateMoviesListInDb(topMoviesFromFile);
+      /**---------- after mapping set and store the list with names and number of occurrences of genres in the list ------------*/
+      await TopMovie.setGenresList();
 
       res.status(200).json({
           message: `Finished`,
--- utils/update-functions-utils.ts
+++ utils/update-functions-utils.ts
@@ -1,10 +1,71 @@
 import {appendFile} from "fs/promises";
-import {NotAddedMovie} from "../types";
+import {MoviesFromOMDBAPI, NotAddedMovie, TopMoviesInterface} from "../types";
+import {TopMovie} from "../records/topMovies.record";
+import {fetchFunction} from "./fetchFunction";
 
 
+export const updateMoviesListInDb = async (topMovies: TopMoviesInterface[] )=>{
+
+  /**---------- map the list of movies ------------*/
+  await Promise.all(topMovies.map(async (movie) => {
+      const {position, origTitle, polTitle, year, imgOfMovie} = movie;
+      /**---------- check if movie is in my DB ------------*/
+      const movieFromDB = year && (origTitle || polTitle) ? await TopMovie.checkIfItIsInDataBase(new Date(year), origTitle, polTitle): null;
+      /**---------- if it is, update only the position  ------------*/
+      if(movieFromDB){
+        return movieFromDB.position !== Number(position) ?
+          await TopMovie.updateMoviePosition(movieFromDB.id, Number(position), movieFromDB.origTitle) :
+          origTitle;
+      } else {
+
+        /**---------- if movie is not in DB fetch movie data from OMDB API ------------*/
+        let movieFromOMDB = (await fetchFunction('GET', `${encodeURIComponent(movie.origTitle || movie.polTitle)}`, `${movie.year}`) as unknown as MoviesFromOMDBAPI);
+
+        /**---------- if movie is in OMDB DB ------------*/
+        if (movieFromOMDB !== null) {
+          const {
+            Title: title, //lacking of title usage from this source, I'm going to use it to check if data from OMDBAPI is correct
+            Rated: rated,
+            Genre: genre,
+            Director: director,
+            Actors: actors,
+            Plot: plot,
+            Poster: poster
+          } = movieFromOMDB;
+
+          /**---------- create instance of TomMovie class ------------*/
+          const newMovie = new TopMovie({
+            rated,
+            genre,
+            director,
+            actors,
+            plot,
+            poster,
+            position: Number(position),
+            origTitle: origTitle || polTitle,
+            polTitle: polTitle || origTitle,
+            year: new Date(year),
+            imgOfMovie,
+          });
+
+          /**---------- and save this instance on my DB and return the title or undefined if it is not saved ------------*/
+          return await newMovie.addNewMovieToDataBase() as string | undefined;
+        }
+
+        /**---------- if above could not be returned add this movie to list of unsuccessful addings ------------*/
+        await addUnsuccessfulMovieAddingToFIle({
+          origTitle: movie.origTitle,
+          polTitle: movie.polTitle,
+        } as NotAddedMovie);
+        return;
+      }
+    }
+  ));
 
 
-export const addUnsuccessfulMovieAddingToFIle = async (movieToAddToListOfNotAdded: NotAddedMovie): Promise<void>=> {
+}
+
+const addUnsuccessfulMovieAddingToFIle = async (movieToAddToListOfNotAdded: NotAddedMovie): Promise<void>=> {
 const {origTitle, polTitle} = movieToAddToListOfNotAdded;
   await appendFile(__dirname+ '/badMovies.txt',
     `could not add movie:\n
